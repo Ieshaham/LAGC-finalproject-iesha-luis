df98c61c05501dbb81336483e315988ed5345b15
lugras <69491958+lugras@users.noreply.github.com>
2023-07-30 02:14:09 +0000

    ajustes_api_explore

--- react-app/src/component/Explore.jsx
+++ react-app/src/component/Explore.jsx
@@ -1,33 +1,85 @@
-
-import React, { useContext, useState } from "react";
-
+import React, { useContext, useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import "../App.css"
+import "../App.css";
 
-// const username  =useState(username,useState );
+const backendHostUrl = `${process.env.REACT_APP_FIREBASE_FUNCTIONS_HOST}/geeks-firebase-72e6d/us-central1`;
+  
+// const backendHostUrl ="https://lugras-fluffy-space-computing-machine-vw5pw6v769qhw64g-3000.preview.app.github.dev";
+
 export default function Explore() {
-    const navigate = useNavigate();
+   const [temp, setTemp] = useState(0);
+  // const [lowtemp, setLowTemp] = useState(0);
+  // const [conditionicon, setConditionIcon] = useState("");
+  // const [condition, setCondition] = useState("");
+  // const [location, setLocation] = useState("");
+  // const [region, setRegion] = useState("");
+  // const [maxtemp, setMaxTemp] = useState("");
+  // const [mintemp, setMinTemp] = useState("");
+  // const [epotch, setEpotch] = useState("");
+  // const [epotch1h, setEpotch1H] = useState("");
+  // const [epotch2h, setEpotch2H] = useState("");
+  // const [hour0, setHour0] = useState("");
+  // const [currenttime, setCurrentTime] = useState("");
 
-    return (
-        
-        <div className="form-group">
-            <label htmlFor="place">Where to go:
-            </label>
-            <input id="place" />
-            <label htmlFor="initialdate">start Date
-            </label>
-            <input id="initialdate" />
-            <label htmlFor="endate">End Date
-            </label>
-            <input id="endate" />
-            <label htmlFor="activity">Activity
-            </label>
-            <input id="activity" />
+  // const [hours, setHours] = useState([]);
 
-            <button className="btn-primary">Button</button>
-            <button className="btn-primary">Button</button>
+  // // const username  =useState(username,useState );
+
+  // const navigate = useNavigate();
+
+  useEffect(() => {
+    (async () => {
+      const res = await fetch(
+        `${backendHostUrl}/getActivities`
+      );
+
+      const data = await res.json();
+
+      console.log(data);
+
+      // console.log("The res: ", data);
+      //  setTemp(data.data.current.temp_f);
+      // setLowTemp();
+      // setConditionIcon("https:" + data.data.current.condition.icon);
+      // setCondition(data.data.current.condition.text);
+      // setLocation(data.data.location.name);
+      // setRegion(data.data.location.region);
+      // setMaxTemp(data.data.forecast.forecastday[0].day.maxtemp_f);
+      // setMinTemp(data.data.forecast.forecastday[0].day.mintemp_f);
+      // setEpotch(data.data.current.condition.last_updated_epoch);
+      // setHour0(data.data.forecast.forecastday[0].hour[0]);
+
+      // setHours(data.data.forecast.forecastday[0].hour);
+    })();
+  }, []);
+
+  return (
+    <div className="form-group row">
+      <div className="row">
+        <label htmlFor="place">Where to go:</label>
+        <input id="place" />
+      </div>
+      <div className="row">
+        <div className="col">
+          <label htmlFor="initialdate">start Date</label>
+          <input id="initialdate" />
         </div>
+        <div className="col">
+          <label htmlFor="endate">End Date</label>
+          <input id="endate" />
+        </div>
+      </div>
+      <div className="row">
+        <label htmlFor="activity">Activity</label>
+        <input id="activity" />
+      </div>
+      <div className="row">
+        <button className="btn-primary " width="50px">
+          Button
+        </button>
 
-    )
+        <button className="btn-primary">Button</button>
+      </div>
+    </div>
+  );
 }
-
