c28b086d3b33bc55998fcc7dcd77ba71b8d73d9e
lugras <69491958+lugras@users.noreply.github.com>
2023-07-31 22:03:33 +0000

    Modification files modification

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,21 +1,19 @@
-
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Explore from "./component/Explore"
-// import ExploreDetail from "./component/Exploredetail"
-// import SignIn from "./component/Signin"
-// import SignUp from "./component/Signup"
 import Landingpage from "./component/Landingpage"
+import Explore from "./component/Explore"
+import SignIn from "./component/Signin"
+// import SignUp from "./component/Signup"
+// import ExploreDetail from "./component/Exploredetail"
 
 export default function App() {
   return (
     <BrowserRouter>
-      
         <Routes>
           <Route path="/" element={<Landingpage />}></Route>
           <Route path="/explore" element={<Explore />}></Route>
-          {/* <Route path="/exploredetails" element={<ExploreDetail />}></Route>
           <Route path="/signin" element={<SignIn />}></Route>
+          {/* <Route path="/exploredetails" element={<ExploreDetail />}></Route>
           <Route path="/signup" element={<SignUp />}></Route> */}
         </Routes>
   
