a3f128bca0da35b4f6da98f93df5f22869b52f36
Ieshaham <131932350+Ieshaham@users.noreply.github.com>
2023-08-02 23:19:09 +0000

    details page started

---
+++ react-app/src/component/Exploredetail.jsx
@@ -0,0 +1,11 @@
+import React, { useContext, useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import "../App.css";
+
+
+export default function Exploredetail() {
+
+    return (
+        <div className='title'>Voyage</div>
+    );
+}
