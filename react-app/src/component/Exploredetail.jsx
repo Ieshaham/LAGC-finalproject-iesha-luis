efce9a999ae801920afdcafa41037dee1359913a
lugras <69491958+lugras@users.noreply.github.com>
2023-08-03 22:32:25 +0000

    cambios exploredetail

--- react-app/src/component/Exploredetail.jsx
+++ react-app/src/component/Exploredetail.jsx
@@ -1,9 +1,22 @@
+import React, { useState } from "react";
 import "../App.css";
 // import React, { useState } from 'react';
 // import axios from 'axios';
 import { Link } from 'react-router-dom';
 export default function App() {
 
+export default function ExploreDetail(){
+
+
+    
+    return (
+    
+        <div className="details">
+            
+        </div>
+    
+    );
+};
 
  return (
 <div className="details">
