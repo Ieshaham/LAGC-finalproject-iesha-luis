6abfa841c93cb73006df8e038261437c6303879b
lugras <69491958+lugras@users.noreply.github.com>
2023-08-04 02:01:31 +0000

    modifications geolocation page

--- react-app/src/component/Geolocation.jsx
+++ react-app/src/component/Geolocation.jsx
@@ -1,26 +1,26 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import axios from "axios";
 // import { CityToLatLngConverter } from './Geolocation';
 
 
-const CityToLatLngConverter = () => {
+export default function CityToLatLngConverter  () {
   const [city, setCity] = useState("");
   const [latitude, setLatitude] = useState("");
   const [longitude, setLongitude] = useState("");
+  let latiduds=0;
+  let longituds=0;
 
+  const backendHostUrl = `${process.env.REACT_APP_FIREBASE_FUNCTIONS_HOST}/geeks-firebase-72e6d/us-central1`;
 
   const handleCityChange = (event) => {
     setCity(event.target.value);
   };
 
   const getLatLng = () => {
-    // const apiKey = `${process.env.REACT_APP_GOOGLE_PLACES_API_KEY}`;
-    const apiKey = `AIzaSyBkZdDeKGgnKXzAu-RWrDGQAQBG-x3D334`;
+    const apiKey = `${process.env.REACT_APP_GOOGLE_PLACES_API_KEY}`;
+    //const apiKey = `AIzaSyBkZdDeKGgnKXzAu-RWrDGQAQBG-x3D334`;
     const geocodeApiUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${city}&key=${apiKey}`;
     console.log(city);
-    console.log(apiKey);
-    
-
     console.log(apiKey);
     console.log(geocodeApiUrl);
 
@@ -31,6 +31,41 @@ const CityToLatLngConverter = () => {
           const { lat, lng } = response.data.results[0].geometry.location;
           setLatitude(lat);
           setLongitude(lng);
+          latiduds=lat;
+          longituds=lng;
+          console.log(latiduds);
+          console.log(longituds);
+          
+          const geocodeApiUrl2 =`https://maps.googleapis.com/maps/api/place/nearbysearch/json?inpu=${city}&types=establishment&location=${latiduds},${longituds}&radius=5000&key=${apiKey}`;
+          console.log(apiKey);
+          console.log(geocodeApiUrl2);
+    axios
+          .get(geocodeApiUrl2)
+         .then((response) => {
+            if (response.data.results.length > 0) {
+
+              {/*const [hours, setHours] = useState([]);
+  
+              useEffect(() => {
+              }, []);*/}
+              
+              
+              (async () => {
+                const results = await fetch(
+                  `${backendHostUrl}/getActivities`
+                );
+          
+                const data = await results.json();
+          
+                console.log(data);
+              })();
+           } else {
+             console.log("Answer not found.");
+             }
+             })
+             .catch((error) => {
+               console.error("Error fetching data: ", error);
+               });
         } else {
           setLatitude("");
           setLongitude("");
@@ -42,36 +77,6 @@ const CityToLatLngConverter = () => {
       });
 
 
-      const geocodeApiUrl2 =`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${setLatitude},${setLongitude}&radius=Autocomplete`;
-      console.log(apiKey);
-      console.log(geocodeApiUrl2);
-axios
-      .get(geocodeApiUrl2)
-     .then((response) => {
-        if (response.data.results.length > 0) {
-       } else {
-         console.log("Answer not found.");
-         }
-         })
-         .catch((error) => {
-           console.error("Error fetching data: ", error);
-           });
-           
-           {/*var axios = require('axios');
-               var config = {
-                 method: 'GET',
-                 url: geocodeApiUrl2,
-                 headers: { }
-               };
-               console.log('gettin Answer');
-               
-               axios(config)
-               .then(function (response) {
-                 console.log(JSON.stringify(response.data));
-               })
-               .catch(function (error) {
-                 console.log(error);
-               });*/}
   };
 
   return (
@@ -94,4 +99,4 @@ axios
   );
 };
 
-export default CityToLatLngConverter;
+//export default CityToLatLngConverter;
