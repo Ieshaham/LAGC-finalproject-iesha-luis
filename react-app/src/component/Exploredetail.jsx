db569fc69ee08bd81dae2a70a78d2d07da3f361e
lugras <69491958+lugras@users.noreply.github.com>
2023-08-13 21:32:31 +0000

    including new pages

--- react-app/src/component/Exploredetail.jsx
+++ react-app/src/component/Exploredetail.jsx
@@ -1,116 +1,57 @@
-
-
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios"; 
 import "../App.css";
-import React, { useState} from 'react';
-
 
 const backendHostUrl = `${process.env.REACT_APP_FIREBASE_FUNCTIONS_HOST}/geeks-firebase-72e6d/us-central1`;
 
 const ExploreDetail = () => {
-  const [city,setCity]= useState("")
-  
-  const [latitude, setLatitude] = useState("");
-  const [longitude, setLongitude] = useState("");
-  let latiduds = 0;
-  let longituds = 0;
-  
+  const queryParameters = new URLSearchParams(window.location.search);
+  const place_id = queryParameters.get("place_id");
+  console.log("The response from previous URL", place_id);
 
-  const handleCityChange = (event) => {
-    setCity(event.target.value);
-  
+  let navigate = useNavigate();
+  const goBack = () => {
+    let path = `/geolocation`;
+    navigate(path);
   };
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const apiKey = `${process.env.REACT_APP_GOOGLE_PLACES_API_KEY}`;
+        const response = await axios.get(
+          `https://maps.googleapis.com/maps/api/place/details/json?place_id=${place_id}&key=${apiKey}`
+        );
 
-  const getLatLng = async () => {
+        if (response.status === 200) {
+          const data2 = response.data; 
+          console.log(data2);
+          
+        } else {
+          console.error("Error fetching place detail:", response.status, response.statusText);
+        }
+      } catch (error) {
+        console.error("Error fetching place detail:", error);
+      }
+    };
 
-
-    const res = await fetch(`${backendHostUrl}/getCoordinates`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ city }),
-    });
-
-
-    const data = await res.json();
-    console.log(data);
-    console.log(data.city);
-
- 
-    const { lat, lng } = data.data.results[0].geometry.location;
-
-    const activityRes = await fetch(`${backendHostUrl}/getActivities`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ lat, lng }),
-    });
-
-    const placesData = await activityRes.json();
-    console.log(placesData);
-  };
+    fetchData();
+  }, [place_id]);
 
   return (
-
-
-
-      <div className="form-group">
-        <input
-          type="text"
-          placeholder="Enter city name"
-          value={city}
-          onChange={handleCityChange}
-        />
-        <button onClick={getLatLng}>Get Latitude and Longitude</button>
-        {latitude && longitude && (
-          <div>
-            Latitude: {latitude}
-            <br />
-            Longitude: {longitude}
-          
-          </div>
-          
-        )}
+    <div className="form-group">
+      <div>
+        <label>The place_id is:</label>
+        {place_id}
       </div>
-    );
-  }
-  export default ExploreDetail;
+      <div>
+        <button className="btn btn-primary" onClick={goBack}>
+          &larr;Go back
+        </button>
+      </div>
+    </div>
+  );
+};
 
-
-
-
-
-// import React, { useState, useEffect } from 'react';
-
-// import List from './List'; // Assuming you have a List component
-
-// const App = () => {
-//   const [places, setPlaces] = useState([]);
-//   const apiKey = process.env.REACT_APP_GOOGLE_PLACES_API_KEY;
-
-//   useEffect(() => {
-//     async function fetchPlaces() {
-//       try {
-//         const response = await fetch(`https://maps.googleapis.com/places/api/js?key=${apiKey}`);
-//         setPlaces(response.data.results);
-//       } catch (error) {
-//         console.error('Error fetching places:', error);
-//       }
-//     }
-
-//     fetchPlaces();
-//   }, []);
-
-//   return (
-//     <div>
-//       <List places={places} />
-//     </div>
-//   );
-// };
-
-// export default App;
-
-
-  
+export default ExploreDetail;
