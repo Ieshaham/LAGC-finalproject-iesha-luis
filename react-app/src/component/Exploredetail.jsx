d33d491440d3808955660e2d042655d232743663
lugras <69491958+lugras@users.noreply.github.com>
2023-08-15 00:51:39 +0000

    modifiying exploredetail and removins components

--- react-app/src/component/Exploredetail.jsx
+++ react-app/src/component/Exploredetail.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import axios from "axios"; 
 import "../App.css";
 
 const backendHostUrl = `${process.env.REACT_APP_FIREBASE_FUNCTIONS_HOST}/geeks-firebase-72e6d/us-central1`;
@@ -8,6 +7,14 @@ const backendHostUrl = `${process.env.REACT_APP_FIREBASE_FUNCTIONS_HOST}/geeks-f
 const ExploreDetail = () => {
   const queryParameters = new URLSearchParams(window.location.search);
   const place_id = queryParameters.get("place_id");
+  const [Phone, setPhone] = useState("");
+  const [Name, setName] = useState("");
+  const [Photo, setPhoto] = useState("");
+  const [Url, setUrl] = useState("");
+  const [Address, setAddress] = useState("");
+  const [Website, setWebsite] = useState("");
+  
+  const [PlaceDetail, setPlaceDetail] = useState([]);
   console.log("The response from previous URL", place_id);
 
   let navigate = useNavigate();
@@ -29,7 +36,13 @@ const ExploreDetail = () => {
   
     const data2 = await res2.json();
     console.log("This is the response:", data2);
-  
+    setPhone(data2.data.result.formatted_phone_number);
+    setName(data2.data.result.name);
+    setPhoto(data2.data.result.photos[0].photo_reference);
+    setUrl(data2.data.result.url);
+    setAddress(data2.data.result.vicinity);
+    setWebsite(data2.data.result.website);
+    //console.log(Phone);
   
   console.log("Out from call function getPlaceDetail");
   
@@ -42,8 +55,22 @@ const ExploreDetail = () => {
   return (
     <div className="form-group">
       <div>
+        <h1>{Name}</h1>
+        <div>The photo reference:
+          {Photo}
+        </div>
+        <div>Google Maps Location: {Url}</div>
         <label>The place_id is:</label>
         {place_id}
+        </div>
+      <div>Address:
+        {Address}
+      </div>
+        <div>
+        Phone #:{Phone}
+      </div>
+      <div>
+        For more information visit: {Website}
       </div>
       <div>
         <button className="btn btn-primary" onClick={goBack}>
