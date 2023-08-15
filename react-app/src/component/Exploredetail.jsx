4e96c6eecc25d8594bfd43b1e668b6eb5356aa0a
lugras <69491958+lugras@users.noreply.github.com>
2023-08-15 23:38:51 +0000

    chages exploredetail

--- react-app/src/component/Exploredetail.jsx
+++ react-app/src/component/Exploredetail.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "../App.css";
 
 const backendHostUrl = `${process.env.REACT_APP_FIREBASE_FUNCTIONS_HOST}/geeks-firebase-72e6d/us-central1`;
@@ -13,8 +14,7 @@ const ExploreDetail = () => {
   const [Url, setUrl] = useState("");
   const [Address, setAddress] = useState("");
   const [Website, setWebsite] = useState("");
-  
-  const [PlaceDetail, setPlaceDetail] = useState([]);
+
   console.log("The response from previous URL", place_id);
 
   let navigate = useNavigate();
@@ -33,7 +33,7 @@ const ExploreDetail = () => {
       },
       body: JSON.stringify({ place_id }),
     });
-  
+
     const data2 = await res2.json();
     console.log("This is the response:", data2);
     setPhone(data2.data.result.formatted_phone_number);
@@ -43,41 +43,56 @@ const ExploreDetail = () => {
     setAddress(data2.data.result.vicinity);
     setWebsite(data2.data.result.website);
     //console.log(Phone);
-  
-  console.log("Out from call function getPlaceDetail");
-  
+
+    console.log("Out from call function getPlaceDetail");
   };
-  
+
   useEffect(() => {
     getPlaceDetail();
-  }, [])
+  }, []);
 
   return (
-    <div className="form-group">
-      <div>
-        <h1>{Name}</h1>
-        <div>The photo reference:
-          {Photo}
-        </div>
-        <div>Google Maps Location: {Url}</div>
-        <label>The place_id is:</label>
-        {place_id}
-        </div>
-      <div>Address:
-        {Address}
-      </div>
-        <div>
-        Phone #:{Phone}
-      </div>
-      <div>
-        For more information visit: {Website}
-      </div>
-      <div>
-        <button className="btn btn-primary" onClick={goBack}>
-          &larr;Go back
-        </button>
-      </div>
-    </div>
+    <table className="table">
+      <thead>
+        <tr>
+          <h1>The place information</h1>
+        </tr>
+        <tr>
+          <th>Name</th>
+          <th>Phone</th>
+          <th>Address</th>
+          <th>Website</th>
+          <th>Google Maps Location</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>
+            {Name}
+            The photo reference:
+            {Photo}
+            {place_id}
+          </td>
+          <td>{Phone}</td>
+          <td>
+            {Address}
+          </td>
+          <td>
+          <Link to={`${Website}`}>Website</Link>
+          </td>changes
+          <td>
+            <Link to={`${Url}`}>Google Maps Location</Link>
+          </td>
+        </tr>
+        <tr>
+          <div>
+            <button className="btn btn-primary" onClick={goBack}>
+              &larr;Go back
+            </button>
+          </div>
+        </tr>
+      </tbody>
+    </table>
   );
 };
 
--- react-app/src/component/Geolocation.jsx
+++ react-app/src/component/Geolocation.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
+import "../App.css";
 
 export default function CityToLatLngConverter() {
   const [city, setCity] = useState("");
