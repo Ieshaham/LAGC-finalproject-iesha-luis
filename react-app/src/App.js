4679e011ad037e003c50e605286422c16a36bee1
Ieshaham <131932350+Ieshaham@users.noreply.github.com>
2023-08-16 20:56:32 -0400

    Update App.js

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,10 +2,9 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landingpage from "./component/Landingpage"
 import Explore from "./component/Explore"
-// import SignInG from "./component/Signing"
-import SignUP from "./component/Signup"
 import List from "./component/List"
-
+import ContextProvider from "./Context";
+import NavBar from "./component/NavBar";
 
 
 import ExploreDetail from "./component/Exploredetail"
@@ -19,19 +18,17 @@ import Map from "./component/Map";
 export default function App() {
   return (
     <BrowserRouter>
+      <ContextProvider>
+      <NavBar />
       <Routes>
         <Route path="/" element={<Landingpage />}></Route>
         <Route path="/explore" element={<Explore />}></Route>
-{/* 
-         <Route path="/signing" element={<SignInG />}></Route> */}
-        <Route path="/signup" element={<SignUP />}></Route> 
         <Route path="/geolocation" element={<CityToLatLngConverter />}></Route>
         <Route path="/exploredetail" element={<ExploreDetail />}></Route>
         <Route path="/map" element={<Map />}></Route>
         <Route path="/list" element={<List/>}></Route>
-        
       </Routes>
-
+      </ContextProvider>
     </BrowserRouter>
   );
 }
