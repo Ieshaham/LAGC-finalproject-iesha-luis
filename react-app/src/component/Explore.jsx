d2f5c1eb5d4f0223f81098a245329bece0a60215
Ieshaham <131932350+Ieshaham@users.noreply.github.com>
2023-08-18 05:48:18 +0000

    style the explore page

--- react-app/src/component/Explore.jsx
+++ react-app/src/component/Explore.jsx
@@ -9,35 +9,35 @@ const backendHostUrl = `${process.env.REACT_APP_FIREBASE_FUNCTIONS_HOST}/geeks-f
 // const backendHostUrl ="https://lugras-fluffy-space-computing-machine-vw5pw6v769qhw64g-3000.preview.app.github.dev";
 
 export default function Explore() {
-  let navigate = useNavigate(); 
-  const routeChangetoList = () =>{ 
-    let path = `/list`; 
+  let navigate = useNavigate();
+  const routeChangetoList = () => {
+    let path = `/list`;
     navigate(path);
   }
-  const routeChangetoGeolocation = () =>{ 
-    let path = `/geolocation`; 
+  const routeChangetoGeolocation = () => {
+    let path = `/geolocation`;
     navigate(path);
   }
   return (
-    <div className="form-group row">
+    <div className="form-group-row">
       <div>
         <h1>Search your places</h1>
       </div>
       <div>
         <p>
-          Here you could choose search by an specific City Name or you could get
-          more information about an specific place. Please choose the best
+          Here you could choose search by an specific City name or you could personalize
+          a travel Itinerary. Please choose the best
           option for your search.
         </p>
       </div>
       <div className="row">
         <div>
-          <button className="btn  btn-success" width="120px" onClick={routeChangetoGeolocation}>
+          <button className="seach-city" width="120px" onClick={routeChangetoGeolocation}>
             Search by City
           </button>
         </div>
         <div>
-          <button className="btn btn-warning" width="120px" onClick={routeChangetoList}>
+          <button className="search-place" width="120px" onClick={routeChangetoList}>
             Search by Place
           </button>
         </div>
